858f9ae3656b4c3a0d42dea36f2e342431182932
Alex Bunnell and William Kinzig <william@>
2018-06-06 10:25:33 -0700

    Add JS to handle single letter words and add way to vowels.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 var pigLatin = function(word) {
   var theInput = word.split("");
-  return theInput;
+  var vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+  if (theInput.length === 1) {
+      for (var index = 0; index < vowelsArray.length; index++) {
+        if (theInput[0] === vowelsArray[index]) {
+          theInput.push("way");
+        }
+      }//evaluate the array against each vowel in vowelsArray and then add "way" to the letter.
+  }
+  var theResult = theInput.join("");
+  return theResult;
 };
 
 

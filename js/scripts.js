a53ed0a600e63018983f25a59f2bb25f6050a2af
Alex Bunnell and William Kinzig <william@>
2018-06-06 11:56:46 -0700

    Add spec for single word that starts with a vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
-var pigLatin = function(word) {
+var pigLatin = function(word)
+{
   var theInput = word.split("");
   var vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
-  if (theInput.length === 1) {
-      for (var index = 0; index < vowelsArray.length; index++) {
-        if (theInput[0] === vowelsArray[index]) {
+  if (theInput.length === 1)
+  { //for single letter words
+      for (var index = 0; index < vowelsArray.length; index++)
+      {
+        if (theInput[0] === vowelsArray[index])
+        {
           theInput.push("way");
         }
-      }//evaluate the array against each vowel in vowelsArray and then add "way" to the letter.
-  }
-  var theResult = theInput.join("");
-  return theResult;
+      }
+      var theResult = theInput.join("");
+      return theResult;
+  } else if (theInput.length != 1)
+    { //word is longer than 1 character
+      for (var index = 0; index < vowelsArray.length; index++)
+      {
+        if (theInput[0] === vowelsArray[index])
+        {
+          theInput.push("way"); //if word starts with vowel, just add way
+        } else
+        {
+          theInput.push(theInput[0]);
+          theInput.splice(0, 1);
+          theInput.push("ay");
+          index = vowelsArray.length;
+          var theResult = theInput.join("");
+          return theResult;
+        }
+      }
+    }
 };
 
 
